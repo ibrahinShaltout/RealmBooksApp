f9bfb5a79f7441ba9c6e20f793f4a8f6b1cad0b0
Ibrahim Shaltout <40940509+ibrahinShaltout@users.noreply.github.com>
2019-12-06 12:13:10 +0200

    update

---
+++ app/src/main/java/com/example/realmbooksapp/adapters/RealmBooksAdapter.java
@@ -0,0 +1,15 @@
+package com.example.realmbooksapp.adapters;
+
+import android.content.Context;
+
+import com.example.realmbooksapp.model.Book;
+
+import io.realm.RealmResults;
+
+public class RealmBooksAdapter extends RealmModelAdapter<Book> {
+
+    public RealmBooksAdapter(Context context, RealmResults<Book> realmResults, boolean automaticUpdate) {
+
+        super(context, realmResults, automaticUpdate);
+    }
+}
---
+++ app/src/main/java/com/example/realmbooksapp/adapters/RealmRecyclerViewAdapter.java
@@ -0,0 +1,27 @@
+package com.example.realmbooksapp.adapters;
+
+
+import androidx.recyclerview.widget.RecyclerView;
+
+import io.realm.RealmBaseAdapter;
+import io.realm.RealmObject;
+
+public abstract class RealmRecyclerViewAdapter<T extends RealmObject> extends RecyclerView.Adapter {
+
+    private RealmBaseAdapter<T> realmBaseAdapter;
+
+    public T getItem(int position) {
+
+        return realmBaseAdapter.getItem(position);
+    }
+
+    public RealmBaseAdapter<T> getRealmAdapter() {
+
+        return realmBaseAdapter;
+    }
+
+    public void setRealmAdapter(RealmBaseAdapter<T> realmAdapter) {
+
+        realmBaseAdapter = realmAdapter;
+    }
+}
